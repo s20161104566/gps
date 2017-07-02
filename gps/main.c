0425b4fd2b44fcc30ed7574b22584da30dcf62d8
s20161104566 <s20161104566@admindeiMac-35.local>
2017-07-02 20:07:58 +0800

     gps
    修改，增添

--- gps/main.c
+++ gps/main.c
@@ -10,72 +10,92 @@
 int main(int args,const char * argv[]) {
     FILE *fr;
     FILE *fr2;
-    char time[8],str1[70],str2[70],lat[12],lg[15],spr[7],gro[7],date[8],high[5];
-    int i,year,month,day,hour,minute,second;
+    char str1[70],str2[70],lat[12],lg[15],spr[7],gro[7],high[5];
+    char year[10],month[10],day[6],hour[10],min[10],second[10];
+    int i;
     fr=fopen("//Users//s20161104566//Desktop//GPS170510.log","r+" );
-    fr2=fopen("//Users//s20161104566//Desktop//gps//output.txt","w+" );
-    fscanf(fr,"%s%s",str1,str2);
-    printf("结果: %s\n%s\n",str1,str2);
-    fprintf(fr2,"结果: %s\n%s\n",str1,str2);
-    for(i=0;i<8;i++)
-        time[i]=str1[i+7];
-    time[7]='\0';
-    printf("时间:%s\n",time);
-    fprintf(fr2,"时间:%s\n",time);
-    for(i=0;i<12;i++)
-        lat[i]=str1[i+16];
-    lat[11]='\0';
-    printf("纬度:%s\n",lat);
-    fprintf(fr2,"纬度:%s\n",lat);
-    for(i=0;i<12;i++)
-        lg[i]=str1[i+28];
-    lg[11]='\0';
-    printf("经度:%s\n",lg);
-    fprintf(fr2,"经度:%s\n",lg);
-    for(i=0;i<6;i++)
-        spr[i]=str1[i+39];
-    spr[6]='\0';
-    printf("速率:%s\n",spr);
-    fprintf(fr2,"速率:%s\n",spr);
-    for(i=0;i<7;i++)
-        gro[i]=str1[i+45];
-    gro[6]='\0';
-    printf("地面航向:%s\n",gro);
-    fprintf(fr2,"地面航向:%s\n",gro);
-
-    for(i=0;i<5;i++)
-        high[i]=str2[i+43];
-    high[4]='\0';
-    printf("海拔:%s千米\n",high);
-    fprintf(fr2,"海拔:%s千米\n",high);
-    fclose(fr);
-    for(i=0;i<8;i++)
-        date[i]=str1[i+51];
-    date[7]='\0';
-    int n,a[10],y,b;
-    n=atoi(date);
-    for(i=0;i<6;i++)
+    fr2=fopen("//Users//s20161104566//Desktop//gps//output.csv","w+" );
+    if(fr==NULL)
     {
-        a[i]=n%10;
-        n=n/10;
+        printf("文件打开错误 , 文件可能不存在\n");
     }
-    y=a[0]+a[1]*10+2000;
-    month=a[3]*10+a[2];
-    day=a[5]*10+a[4];
-    hour=a[5]*10+a[4]+8;
-    minute=a[3]*10+a[2];
-    second=a[1]*10+a[0];
-    b=0;
-    if(hour>=24)
+    else
     {
-        b++;
-        hour=hour-24;
+        printf("gps\n");
+        fprintf(fr2,"gps\n");
+        printf(" 日期,时间,纬度,经度,速率,地面航向,海拔\n");
+        fprintf(fr2," 日期,时间,纬度,经度,速率,地面航向,海拔\n");
+        while(fscanf(fr,"%s %s",str1,str2)!=EOF)
+        {
+            for(i=0;i<2;i++)
+            {
+                year[i]=str1[i+55];
+            }
+            year[2]='\0';
+            for(i=0;i<2;i++)
+            {
+                month[i]=str1[i+53];
+            }
+            month[2]='\0';
+            for(i=0;i<2;i++)
+            {
+                day[i]=str1[i+51];
+            }
+            day[2]='\0';
+            for(i=0;i<2;i++)
+            {
+                hour[i]=str1[i+7];
+            }
+            hour[2]='\0';
+            for(i=0;i<2;i++)
+            {
+                min[i]=str1[i+9];
+            }
+            min[2]='\0';
+            for(i=0;i<2;i++)
+            {
+                second[i]=str1[i+11];
+            }
+            second[2]='\0';
+            for(i=0;i<8;i++)
+                lat[i]=str1[i+16];
+            lat[11]='\0';
+            for(i=0;i<9;i++)
+                lg[i]=str1[i+27];
+            lg[11]='\0';
+            
+            for(i=0;i<5;i++)
+                spr[i]=str1[i+39];
+            spr[6]='\0';
+            
+            for(i=0;i<5;i++)
+                gro[i]=str1[i+45];
+            gro[6]='\0';
+            
+            
+            for(i=0;i<4;i++)
+                high[i]=str2[i+43];
+            high[4]='\0';
+            printf("%s年%s月%s日,%s时%s分%s秒,",year,month,day,hour,min,second);
+            fprintf(fr2,"%s年%s月%s日,%s时%s分%s秒,",year,month,day,hour,min,second);
+            
+            printf("%s,",lat);
+            fprintf(fr2,"%s,",lat);
+            
+            printf("%s,",lg);
+            fprintf(fr2,"%s,",lg);
+            
+            printf("%s,",spr);
+            fprintf(fr2,"%s,",spr);
+            
+            printf("%s,",gro);
+            fprintf(fr2,"%s,",gro);
+            
+            printf("%s千米,",high);
+            fprintf(fr2,"%s千米\n",high);
+        }
+        fclose(fr);
+        fclose(fr2);
     }
-    printf("%s",date);
-    printf("日期:%d\n",y);
-    fprintf(fr2,"日期:%s\n",date);
-    printf (" %d年%d月%d日",year,month,day);
-    printf(" %d时%d分%d秒",hour,minute,second);
-    
     return 0;
 }
